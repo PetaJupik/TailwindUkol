bbf1d5903258166e632c17952dbf0a4b0b3b918e
PetaJupik <111487174+PetaJupik@users.noreply.github.com>
2025-06-01 14:22:56 +0200

    refactor: přeuspořádání struktury projektu a aktualizace souborů
    - Přesunuto index.html, input.css a output.css do složky src.
    - Aktualizace package.json pro nové závislosti a verze.
    - Přidání nových SVG log a ikon do projektu.
    - Odstranění starých souborů a přidání nových stylů pro Tailwind CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,21 +19,10 @@ module.exports = {
 
       fontFamily: {
         sans: [
-          'Inter',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
+          'Work Sans',
+        ],
+        serif: [
+          'Crimson Text',
         ],
 
       },
